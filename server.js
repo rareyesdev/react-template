07834f941c2089ed8bbd8389cd53a16d1771013b
Ramón Alejandro Reyes Fajardo <91agarwaen@gmail.com>
2018-02-16 01:26:05 -0600

    Update server to use static files instead of compiling at runtime

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
+const path = require('path');
 const express = require('express');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const config = require('./webpack/webpack.production.js');
 
 const app = express();
-const compiler = webpack(config);
 
 const port = process.env.PORT || 8080;
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-}));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', port);
 
 // Serve the files on port 3000.
-app.listen(port, () => {
-  console.log(`react-template app listening on port ${port}!\n`);
+const server = app.listen(port, () => {
+  console.log(`react-template app listening on port ${server.address().port}!\n`);
 });

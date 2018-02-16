bb1b0583b787754a63a6e1e01a573aa17ce3aaad
Ramón Alejandro Reyes Fajardo <91agarwaen@gmail.com>
2018-02-16 02:27:14 -0600

    Remove comment

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', port);
 
-// Serve the files on port 3000.
 const server = app.listen(port, () => {
   console.log(`react-template app listening on port ${server.address().port}!\n`);
 });

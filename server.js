4bffdb22c5fc3a060f5bb8d80c4aaedebc8ee248
Ramón Alejandro Reyes Fajardo <91agarwaen@gmail.com>
2018-02-16 00:55:37 -0600

    Update app port. Pick it up from environment if available

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ const config = require('./webpack/webpack.production.js');
 const app = express();
 const compiler = webpack(config);
 
+const port = process.env.PORT || 8080;
+
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
 }));
+app.set('port', port);
 
 // Serve the files on port 3000.
-app.listen(3000, () => {
-  console.log('react-template app listening on port 3000!\n');
+app.listen(port, () => {
+  console.log(`react-template app listening on port ${port}!\n`);
 });

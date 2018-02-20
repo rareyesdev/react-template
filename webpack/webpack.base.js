420fde283eae342d6fa0a16100132a6e1de5b581
Ramón Alejandro Reyes Fajardo <91agarwaen@gmail.com>
2018-02-20 02:42:00 -0600

    Add hashes to files

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -68,14 +68,24 @@ const config = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader'
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]__[hash:7].[ext]'
+            }
+          },
         ],
         include: path.resolve(__dirname, '..', 'src')
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          'file-loader'
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]__[hash:7].[ext]'
+            }
+          },
         ],
         include: path.resolve(__dirname, '..', 'src')
       }
